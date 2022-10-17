724b5c4ec4e1c7254b989098124ded847ced54ae
MaohuaFan <9019910@student.zadkine.nl>
2022-10-17 14:23:03 +0200

    Commit

---
+++ JavascriptW4/app.js
@@ -0,0 +1,10 @@
+
+alert("Hallo Wereld");
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+
+var kleur = prompt("Geef een Engelse kleurnaam: (red of blue", "");
+document.getElementById("kop").style.color = kleur;
+
+document.getElementById("p1").innerHTML = "Hallo " + kleur + '!';
---
+++ js_6.1/script.js
@@ -0,0 +1,31 @@
+/*
+    function name(params) {
+        
+        alert("Hallo Wereld");
+        var getal1 = 12;
+        var getal2 = 4;
+        var som = getal1 * getal2;
+
+        var kleur = prompt("Geef een Engelse kleurnaam: (red of blue", "");
+        document.getElementById("kop").style.color = kleur;
+
+        document.getElementById("p1").innerHTML = "Hallo " + kleur + '!';
+
+    }
+*/
+
+
+function ChangeColor(params) {
+    //hier je de code te staan
+    alert("Het werkt!");
+    console.log("Het werkt in de console!");
+}
+
+function Calc(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+    alert(answer);
+}
+
+Calc(8, 15);
+
