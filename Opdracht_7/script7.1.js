3baff36f1b7ef3fa57ac0c42afe41e0b11a0b5a4
MaohuaFan <9019910@student.zadkine.nl>
2022-10-20 14:27:20 +0200

    6

--- Opdracht_7/script7.1.js
+++ Opdracht_7/script7.1.js
@@ -3,8 +3,10 @@ var product = 60;
 function balance() {    
     if(budget > product) {
         document.getElementById("p1").innerHTML = "U heeft genoeg geld!";
+        document.getElementById("p1").style.color = "green";
     }
     else {
         document.getElementById("p1").innerHTML = "Helaas, te weinig geldt";
+        document.getElementById("p1").style.color = "red";
     }
 }
