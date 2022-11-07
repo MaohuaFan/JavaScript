fe78eaab6e2a75c168ddb07080ca57ad152206cb
MaohuaFan <9019910@student.zadkine.nl>
2022-11-07 14:56:32 +0100

    script 8.2 && is replacedm by ||

--- Opdracht_8/script8.2.js
+++ Opdracht_8/script8.2.js
@@ -3,12 +3,11 @@ function multiply() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
     var som = getal1 * getal2;
-    if(getal1 > 0 && getal2 > 0)
-    {
-        document.getElementById("ans").innerHTML = "Het antwoord is " + som;
+    if (getal1 == 0 || getal2 == 0) {
+        document.getElementById("ans").innerHTML = "Het getal is te laag";
     }
     else {
-        document.getElementById("ans").innerHTML = "Het getal is te laag";
+        document.getElementById("ans").innerHTML = "Het antwoord is " + som;
     }
 }
 
@@ -16,12 +15,11 @@ function minus() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
     var som = getal1 - getal2;
-    if(getal1 > 0 && getal2 > 0)
-    {
-        document.getElementById("ans").innerHTML = "Het antwoord is " + som;
+    if (getal1 == 0 || getal2 == 0) {
+        document.getElementById("ans").innerHTML = "Het getal is te laag";
     }
     else {
-        document.getElementById("ans").innerHTML = "Het getal is te laag";
+        document.getElementById("ans").innerHTML = "Het antwoord is " + som;
     }
 }
 
@@ -29,12 +27,11 @@ function add() {
     var getal1 = parseInt(document.getElementById("getal1").value, 10);
     var getal2 = parseInt(document.getElementById("getal2").value, 10);
     var som = getal1 + getal2;
-    if(getal1 > 0 && getal2 > 0)
-    {
-        document.getElementById("ans").innerHTML = "Het antwoord is " + som;
+    if (getal1 == 0 || getal2 == 0) {
+        document.getElementById("ans").innerHTML = "Het getal is te laag";
     }
     else {
-        document.getElementById("ans").innerHTML = "Het getal is te laag";
+        document.getElementById("ans").innerHTML = "Het antwoord is " + som;
     }
 }
 
@@ -42,11 +39,10 @@ function devide() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
     var som = getal1 / getal2;
-    if(getal1 > 0 && getal2 > 0)
-    {
-        document.getElementById("ans").innerHTML = "Het antwoord is " + som;
+    if (getal1 == 0 || getal2 == 0) {
+        document.getElementById("ans").innerHTML = "Het getal is te laag";
     }
     else {
-        document.getElementById("ans").innerHTML = "Het getal is te laag";
+        document.getElementById("ans").innerHTML = "Het antwoord is " + som;
     }
 }
